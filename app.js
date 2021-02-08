54fca443d042857cd3a778ed6abc64463337f31f
batyapollack <batyapollack6001@gmail.com>
2021-02-08 12:52:44 +0200

    fixed port

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
+var port = process.env.PORT ?? 3000
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -41,7 +41,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(3000, () =>{
+app.listen(port, () =>{
   console.log('server is running');
 })
 
